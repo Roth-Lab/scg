3f5d4dc4d2d86baf5afc388da53151ae549f4b7e
aroth <aroth@localhost>
2015-01-23 14:47:42 -0800

    Added ability to handle multiple data types.

--- bin/simulate.py
+++ bin/simulate.py
@@ -6,15 +6,21 @@ Created on 2015-01-10
 import numpy as np
 
 def simualte_data(alpha_true, gamma_true, kappa_true, G_prior, M, N, inverse_state_map):
+    data_types = G_prior.keys()
+    
     K = len(kappa_true)
     
-    G_true = np.random.multinomial(1, G_prior, size=(K, M)).argmax(axis=2)
+    G_true = {}
+    
+    for data_type in data_types:
+        G_true[data_type] = np.random.multinomial(1, G_prior[data_type], size=(K, M[data_type])).argmax(axis=2)
 
     d_true = np.random.dirichlet(alpha_true)
-
-    e_true = np.array([np.random.dirichlet(gamma_true[0]),
-                       np.random.dirichlet(gamma_true[1]),
-                       np.random.dirichlet(gamma_true[2])])
+    
+    e_true = {}
+    
+    for data_type in data_types:
+        e_true[data_type] = np.array([np.random.dirichlet(x) for x in gamma_true[data_type]])
 
     pi_true = np.random.dirichlet(kappa_true)
 
@@ -23,27 +29,30 @@ def simualte_data(alpha_true, gamma_true, kappa_true, G_prior, M, N, inverse_sta
     Z_true = [np.random.multinomial(1, pi_true, size=N).argmax(axis=1),
               zip(np.random.multinomial(1, pi_true, size=N).argmax(axis=1), np.random.multinomial(1, pi_true, size=N).argmax(axis=1))]
 
-    X = []
-
-    for n in range(N):
-        z = Z_true[Y_true[n]][n]
-
-        if np.isscalar(z):
-            x_n = [np.random.multinomial(1, e_m).argmax() for e_m in e_true[G_true[z]]]
-
-            X.append(x_n)
-
-        else:
-            x = [[np.random.multinomial(1, e_m).argmax() for e_m in e_true[G_true[z[0]]]],
-                 [np.random.multinomial(1, e_m).argmax() for e_m in e_true[G_true[z[1]]]]]
-
-            x_n = []
-
-            for u, v in zip(x[0], x[1]):
-                x_n.append(inverse_state_map[(u,v)])
-
-            X.append(x_n)
-
-    X = np.array(X)
+    data = {}
     
-    return {'d' : d_true, 'e' : e_true, 'pi' : pi_true, 'G' : G_true, 'X' : X, 'Y' : Y_true, 'Z' : Z_true}
+    for data_type in data_types:
+        X = []
+        
+        for n in range(N):
+            z = Z_true[Y_true[n]][n]
+    
+            if np.isscalar(z):
+                x_n = [np.random.multinomial(1, e_m).argmax() for e_m in e_true[data_type][G_true[data_type][z]]]
+    
+                X.append(x_n)
+    
+            else:
+                x = [[np.random.multinomial(1, e_m).argmax() for e_m in e_true[data_type][G_true[data_type][z[0]]]],
+                     [np.random.multinomial(1, e_m).argmax() for e_m in e_true[data_type][G_true[data_type][z[1]]]]]
+    
+                x_n = []
+    
+                for u, v in zip(x[0], x[1]):
+                    x_n.append(inverse_state_map[data_type][(u,v)])
+    
+                X.append(x_n)
+    
+        data[data_type] = np.array(X)
+    
+    return {'d' : d_true, 'e' : e_true, 'pi' : pi_true, 'G' : G_true, 'X' : data, 'Y' : Y_true, 'Z' : Z_true}
