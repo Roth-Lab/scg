c7a68e920dc6fbbfa082ad097189f5beb85ae9fa
Andrew Roth <369899+aroth85@users.noreply.github.com>
2015-01-23 19:06:20 -0800

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+      name='scg',
+      version='0.1.0',
+      description='Python tools for inferring clonal genotypes from single cell data.',
+      author='Andrew Roth',
+      author_email='andrewjlroth@gmail.com',
+      url='http://compbio.bccrc.ca',
+      package_dir = {'': 'lib'},    
+      packages=[ 
+                'scg',
+                ],
+      scripts=['scg']
+     )
